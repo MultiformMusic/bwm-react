b32c5fb1d395b4a431537a40c3b3400ea1fc1b12
michel.dio33@gmail.com <michel.dio33@gmail.com>
2019-04-01 11:07:51 +0200

    auth register server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const config = require('./config/dev');
 const FakeDb = require('./fake-db');
 const rentalRoutes = require('./routes/rentals');
-
+const userRoutes = require('./routes/users');
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(
     () => {
@@ -17,7 +18,9 @@ mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(
 
 const port = process.env.PORT || 3001;
 const app = express();
+app.use(bodyParser.json());
 app.use('/api/v1/rentals', rentalRoutes);
+app.use('/api/v1/users', userRoutes);
 
 app.listen(port, function() {
     console.log("Running server");
